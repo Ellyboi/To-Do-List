d19e420cd08596231aa4d3e68a46330fb81eb0c6
Ellyboi <vitoesitemitope@gmail.com>
2023-03-30 19:24:29 +0100

    all corrections made

---
+++ dist/index.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+(self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || []).push([[0],[
+/* 0 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const myArray = ["Ayo", "Tayo"]
+
+console.log(myArray);
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (myArray);
+
+/***/ })
+],
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(0));
+/******/ }
+]);
