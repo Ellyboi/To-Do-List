bb30587df401ed1b79a0cd4f44766ae7dd4350a0
Ellyboi <vitoesitemitope@gmail.com>
2023-03-31 01:32:30 +0100

    update the webpack config,js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'none',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
-      {
-        test: /\.svg$/,
-        use: 'svg-inline-loader',
-      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(js)$/,
-        use: 'babel-loader',
-      },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+  optimization: {
+    runtimeChunk: 'single',
   },
-  plugins: [new HtmlWebpackPlugin()],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
