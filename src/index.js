2967e1d3e759606df974e32d1cc27d1a7d5c5e7e
Ellyboi <vitoesitemitope@gmail.com>
2023-03-31 02:51:24 +0100

    add modules by having seperate files for functionality purpose

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 /* Imports */
 import './styles.css';
+import {
+  deployList, addToList, editList, removeList,
+} from './modules/functionality.js';
 
 const activitiesToDo = [
   {
-    description: 'First To Do List',
+    description: 'Reading JS Documentation',
     completed: false,
     id: 1,
   },
   {
-    description: 'Second To Do List',
+    description: 'Reading Bootstrap Documentaion',
     completed: false,
     id: 2,
   },
   {
-    description: 'Third To Do List',
+    description: 'DOM Documentaion',
     completed: false,
     id: 3,
   },
@@ -35,3 +38,38 @@ const showTasks = () => {
 };
 
 showTasks();
+
+const taskList = document.getElementById('task-list');
+const newTask = document.getElementById('task-input');
+const submit = document.getElementById('submit-icon');
+
+/* Add To List */
+newTask.addEventListener('keypress', (e) => {
+  addToList(e);
+});
+
+/* Add to List (clicked) */
+submit.addEventListener('click', () => {
+  addToList('clicked');
+});
+
+/* Delete Task */
+taskList.addEventListener('click', (event) => {
+  const clickedItem = event.target.classList[event.target.classList.length - 1];
+  const li = event.target.parentElement;
+  if (clickedItem === 'delete-task') {
+    removeList(li.id);
+    event.target.parentElement.remove();
+  }
+});
+
+/* Edit Task */
+taskList.addEventListener('keypress', (event) => {
+  const pressedItem = event.target.classList[event.target.classList.length - 1];
+  const li = event.target.parentElement;
+  if (pressedItem === 'task-edit') {
+    editList({ index: li.id, event });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', deployList());
